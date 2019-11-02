99b831e92cf679dae71845e6e54074d822a5ff5c
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2019-11-03 01:10:14 +0600

    Added 1 Two Sum

---
+++ easy/1 Two Sum.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # we are taking an object and storing the number that are already checked.
+        # We are using object to search checked numbers in O(1) time
+        iterated_values = {}
+        for i, num in enumerate(nums):
+            check_num = target - num
+            if check_num in iterated_values:
+                return [iterated_values[check_num], i]
+            else:
+                iterated_values[num] = i
+
+
+solution = Solution()
+print(solution.twoSum([2, 7, 11, 15], 9))
