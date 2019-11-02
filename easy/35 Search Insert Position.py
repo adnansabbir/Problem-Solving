84e79d1f5a60ad6acfa1bceb7559dab21ac09c0c
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2019-11-03 03:05:45 +0600

    added 35 Search Insert Position

---
+++ easy/35 Search Insert Position.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+# Implemented Binary search as a sorted array is given as input
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+
+        if nums[start]>target:
+            return start
+        elif nums[end]<target:
+            return end+1
+
+        while start <= end:
+            mid = (start + end) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid - 1
+        return start
+
+
+solution = Solution()
+print(solution.searchInsert([1, 3, 3, 5, 5, 5, 6, 8, 9, 9], 7))
+
+# Time Complexity - > O(log(n))
