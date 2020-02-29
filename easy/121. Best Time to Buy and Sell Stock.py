15cde66819167b16489491301ac716291eac62bb
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2020-02-29 22:29:46 +0600

    Added 122. Best Time to Buy and Sell Stock

---
+++ easy/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+
+        min_price = prices[0]
+        max_profit = 0
+
+        for price in prices[1:]:
+            if price < min_price:
+                min_price = price
+
+            if price - min_price > max_profit:
+                max_profit = price - min_price
+
+        return max_profit
+
+
+sol = Solution()
+print(sol.maxProfit([7, 15, 1, 5, 3, 6, 4]))
