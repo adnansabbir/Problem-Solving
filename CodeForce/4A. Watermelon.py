ee917d755a02319f9b6710d2fd24b289189914ce
adnan.sabbir <adnan.sabbir@selise.ch>
2020-03-02 13:35:56 +0600

    Added 4A Watermelon

---
+++ CodeForce/4A. Watermelon.py
@@ -0,0 +1,9 @@
+def is_even_check():
+    num = int(input())
+
+    print('YES' if (num % 2 == 0 and num > 2) else 'NO')
+
+
+if __name__ == '__main__':
+    is_even_check()
+    # print('NO' if sum(total) else 'YES')
