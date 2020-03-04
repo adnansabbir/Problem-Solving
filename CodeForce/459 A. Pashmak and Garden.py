01f81dc5116aa750798e15e931908c680e9c64a7
adnan.sabbir <adnan.sabbir@selise.ch>
2020-03-04 21:06:36 +0600

    Updated 459A - Pashmak and Garden

--- CodeForce/459 A. Pashmak and Garden.py
+++ CodeForce/459 A. Pashmak and Garden.py
@@ -2,21 +2,17 @@ def pashmak_and_garden():
     coordinates = [int(c) for c in input().split(' ')]
     x1, y1, x2, y2 = coordinates[0], coordinates[1], coordinates[2], coordinates[3]
 
-    Xd = (x1 - x2)
-    Yd = (y1 - y2)
+    Xd = abs(x1 - x2)
+    Yd = abs(y1 - y2)
 
-    result = []
     if Xd and not Yd:
-        result = [x1, y1 + abs(Xd), x2, y2 + abs(Xd)]
+        return [x1, y1 + Xd, x2, y2 + Xd]
     elif Yd and not Xd:
-        result = [x1 + abs(Yd), y1, x2 + abs(Yd), y2]
-    elif Xd != 0 and Yd != 0:
-        result = [x1, y2, x2, y1]
-
-    if Xd != 0 and Yd != 0 and abs(Xd) != abs(Yd):
+        return [x1 + Yd, y1, x2 + Yd, y2]
+    elif Xd == Yd and Xd != 0:
+        return [x1, y2, x2, y1]
+    else:
         return [-1]
-
-    return result
 
 
 if __name__ == '__main__':
