69de7193ff22b9096512538a60da646294117ce5
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2020-03-21 02:25:14 +0600

    Added 783

--- Leetcode/783. Minimum Distance Between BST Nodes.py
+++ Leetcode/783. Minimum Distance Between BST Nodes.py
@@ -1,6 +1,7 @@
 from helpers.BST import array_to_bst
 import sys
 
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -9,18 +10,29 @@ class TreeNode:
 
 
 class Solution:
-    def minDiffInBST(self, root: TreeNode) -> int:
-        l_min = r_min = sys.maxsize
+    def minDiffInBST(self, root: TreeNode, extra_data=None) -> int:
+
+        if not extra_data:
+            extra_data = {
+                'last_visited': None,
+                'minimum_val': sys.maxsize
+            }
+
         if root.left:
-            l_min = min(abs(root.val - root.left.val), self.minDiffInBST(root.left))
+            self.minDiffInBST(root.left, extra_data)
+
+        if extra_data['last_visited']:
+            extra_data['minimum_val'] = min(abs(extra_data['last_visited'].val - root.val), extra_data['minimum_val'])
+
+        extra_data['last_visited'] = root
 
         if root.right:
-            r_min = min(abs(root.val - root.right.val), self.minDiffInBST(root.right))
+            self.minDiffInBST(root.right, extra_data)
 
-        return min(l_min, r_min)
+        return extra_data['minimum_val']
 
 
-test_array = [4, 2, 6, 10, 20]
+test_array = [10, 5, 15, 1, 8]
 sol = Solution()
 print(sol.minDiffInBST(array_to_bst(test_array)))
 # print(ord('A'))
