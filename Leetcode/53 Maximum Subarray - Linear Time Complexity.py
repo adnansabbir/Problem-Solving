dddf642da47517773262260a86f52b035ffd59a1
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2020-04-03 22:12:04 +0600

    Updated 53 with another approach]

--- Leetcode/53 Maximum Subarray - Linear Time Complexity.py
+++ Leetcode/53 Maximum Subarray - Linear Time Complexity.py
@@ -6,17 +6,30 @@ class Solution:
         if not nums:
             return 0
 
-        max_value = max(nums)
-        current_max = 0
-        for num in nums:
-            current_max += num
-            if current_max < 0 and max_value < 0:
-                current_max = max_value
-            elif current_max < 0:
-                current_max = 0
-            max_value = max(current_max, max_value)
+        max_value = nums[0]
+        current_max = nums[0]
+        for num in nums[1:]:
+            if current_max < 0:
+                current_max = num
+            else:
+                current_max += num
+
+            if current_max > max_value:
+                max_value = current_max
         return max_value
 
 
-solution = Solution()
+# O(n) time O(1)Space
+class Solution2:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = nums[0]
+        for i, num in enumerate(nums[1:]):
+            if num + nums[i] > num:
+                nums[i+1] = num + nums[i]
+            if nums[i+1] > max_sum:
+                max_sum = nums[i+1]
+        return max_sum
+
+
+solution = Solution2()
 print(solution.maxSubArray([-100, 1, 2, 3, 4, 5, 6, -50, 5, 5, 5, 5, 5, 5]))
