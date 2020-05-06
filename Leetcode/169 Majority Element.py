0ef9e9c94082419072e8c29f6edd7e554e8a2308
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2020-05-07 04:25:27 +0600

    Updated 169 Majority Element

--- Leetcode/169 Majority Element.py
+++ Leetcode/169 Majority Element.py
@@ -1,6 +1,7 @@
 from typing import List
 
 
+# O(n) and S(n)
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         elem_count = {}
@@ -16,6 +17,25 @@ class Solution:
                 return num
 
 
-sol = Solution()
-print(sol.majorityElement([2,2,2,3,3]))
 # print(ord('A'))
+
+
+# O(n) and S(1)
+class Solution2:
+    def majorityElement(self, nums: List[int]) -> int:
+        winner, count = nums[0], 0
+
+        for num in nums:
+            if count == 0:
+                winner = num
+                count += 1
+            else:
+                if num != winner:
+                    count -= 1
+                else:
+                    count += 1
+        return winner
+
+
+sol = Solution2()
+print(sol.majorityElement([2, 2, 2, 3, 3]))
