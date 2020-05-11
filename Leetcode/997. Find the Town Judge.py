9612792516a9eba58f76f8881258827917ecae92
Adnan Sabbir <adnan.sabbir2012@gmail.com>
2020-05-11 22:34:27 +0600

    Added 997

---
+++ Leetcode/997. Find the Town Judge.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        trusted_arr = [0] * (N + 1)
+
+        judge = 0
+        for t in trust:
+            trusted_arr[t[1]] += 1
+            trusted_arr[t[0]] -= 1
+
+        for i, t in enumerate(trusted_arr):
+
+            if t == N - 1:
+                judge = i
+
+        return judge or -1
+
+
+sol = Solution()
+print(sol.findJudge(3, [[1, 3], [2, 3], [3, 1]]))
